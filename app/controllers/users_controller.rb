2115f567a9a317cb41e87ea99cbf07c20ca8b4e3
Ubuntu <ubuntu@ip-172-31-13-113.ap-northeast-1.compute.internal>
2021-02-10 07:31:19 +0000

    first submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
 
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -29,16 +29,22 @@ class UsersController < ApplicationController
 
   def followings
     @user = User.find(params[:id])
-    @followings = @user.followings.page(params[:page])
+    @followings = @user.followings.page(params[:page]).per(25)
     counts(@user)
   end
   
   def followers
     @user = User.find(params[:id])
-    @followers = @user.followers.page(params[:page])
+    @followers = @user.followers.page(params[:page]).per(25)
     counts(@user)
   end
 
+  def likes
+    @user = User.find(params[:id])
+    @likeposts = @user.likeposts.page(params[:page]).per(25)
+    counts(@user)
+  end
+  
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,15 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
+  has_many :favorites
+  has_many :likeposts, through: :favorites, source: :micropost
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -25,6 +29,19 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def favorite(micropost)
+      favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def likeposts?(micropost)
+    likeposts.include?(micropost)
   end
   
   def feed_microposts
